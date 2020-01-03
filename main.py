587d0d730b6a88f8f5189a7f924e943b90707222
shannyushan <nyushan.darshan@gmail.com>
2020-01-03 20:42:24 +0545

    added 1st commit for 2nd test of downloader module

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
-# import sys, os
+import sys, os
 from modules.linkresolve import UrlCheck
 from modules.search import SearchAnime
 from modules.choose import Choose
 from modules.episodes import AnimeName
+from modules.downloader import Downloader
 
-# script_path = os.path.dirname(sys.argv[0])
-# for may be importing modules path can be necessary
+script_path = os.path.dirname(sys.argv[0])
 
-url = "https://www.animeout.xyz/"
-resp, st_code = UrlCheck(url).st_code()
-if st_code == 200:
-    print(" Link exists")
-    # inp_name = input("Type the anime name you want to search : ")
-    inp_name = "one punch man"
-    res = SearchAnime(inp_name).search_anime()
-    lists = SearchAnime.soup_extract(res)
 
-    # print(Choose(lists).anime())
-    print(lists)
-    # enumerate turns sequence to pair by numbering; from start=int
-    for k, v in enumerate(lists.keys(), start=1):
-        print(f'{k} ==> Title: {v} || Link: {lists[v]}')
-
-    # choice = int(input("Type the number of anime you want to watch"))
-    # # try:
-    #     # here i want the chooice option but i am stuck
-    #     # and this all choice will be later put on choice module  just foe testing purpose
+# url = "https://www.animeout.xyz/"
+# resp, st_code = UrlCheck(url).st_code()
+# if st_code == 200:
+#     print(" Link exists")
+#     # inp_name = input("Type the anime name you want to search : ")
+#     inp_name = "one punch man"
+#     res = SearchAnime(inp_name).search_anime()
+#     lists = SearchAnime.soup_extract(res)
+#
+#     # print(Choose(lists).anime())
+#     print(lists)
+#     # enumerate turns sequence to pair by numbering; from start=int
+#     for k, v in enumerate(lists.keys(), start=1):
+#         print(f'{k} ==> Title: {v} || Link: {lists[v]}')
+#
+#     # choice = int(input("Type the number of anime you want to watch"))
+#     # # try:
+#     #     # here i want the chooice option but i am stuck
+#     #     # and this all choice will be later put on choice module  just foe testing purpose
 
 
 
 # starting for ep list counter
 
-name = "boruto"
-link = "https://www.animeout.xyz/boruto/"
-print(AnimeName(name, link).episodes())
+# name = "boruto"
+# link = "https://www.animeout.xyz/boruto/"
+# print(AnimeName(name, link).episodes())
+
+link = "https://up1.haylimoviez.info/Sereias/GOT/S08/WEB-DL%20480p%20HayliMoviez/game.of.thrones.s08e01.480p.web.x264-HayliMoviez.mkv"
+resp, fl_size, fl_type = Downloader(link).size_calculate()
+# chunk_size, chunk_list = Downloader.spit_size(fl_size)
+chunk_size = Downloader.spit_size(fl_size)
+
+print(fl_type)
+file_name = input("Type the file name you want to save as : ")
+print(Downloader.extension(fl_type))
+file_ext = Downloader.extension(fl_type)
+fl_path = f'{script_path}/{file_name}.{file_ext}'
+print(f'Saving File as {fl_path}')
+print(Downloader.download_chunks(chunk_size, fl_path, resp))
+
--- modules/downloader.py
+++ modules/downloader.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 import urllib3
 
 global file_size
@@ -12,25 +12,48 @@ class Downloader():
 
     def size_calculate(self):
         link = self.link
-        http = urllib3.PoolManager()
-        # if want to use urllib3 instead of requests
-
-        # req = requests.get(link)
-        # request call takes a little longer
-        # than urllib3 so request is replaced by urllib3
-
-        req = http.request('GET', link)
+        req = requests.get(link, stream=True, verify=False)
         content_length = req.headers['Content-Length']
         content_type = req.headers['content-type']
-        return content_length, content_type
+        return req, content_length, content_type
 
     def spit_size(leng):
         c = 8
-        leng = int(leng)
-        diff = int(leng/8)
-        return leng, diff
+        length = int(leng)
+        diff = int(length / 8)
+        ct_diff = diff
+        # chunks = {}
+        # st = 0
+        # for i in range(1,8):
+        #     diff = ct_diff * i
+        #     chunks[i] = {st: diff}
+        #     st = diff
+        # chunks[8] = {diff: leng}
+        # return ct_diff, chunks
+        return ct_diff
 
-        # header = self.header
-
-    def download_chunks(self):
-        pass
+    def download_chunks(chunk_size, fl_path, resp):
+        handle = open(fl_path, "wb")
+        c = 1
+        for chunk in resp.iter_content(chunk_size=50000):
+            print(f'Got chunk {c}')
+            c = c + 1
+            if chunk:  # filter out keep-alive new chunks
+                handle.write(chunk)
+        return f'Download Complete!!'
+    def extension(fl_type):
+        ext = {
+            "video/mp4": "mp4",
+            "video/mpeg": "mpeg",
+            "video/ogg": "ogg",
+            "video/MPV": "MPV",
+            "video/x-matroska": "mkv",
+            "application/octet-stream": "mkv",
+            "audio/mp3": "mp3",
+            "image/jpeg": "jpg"
+        }
+        try:
+            ext = ext[fl_type]
+        except KeyError:
+            ext = "raw"
+        return ext
